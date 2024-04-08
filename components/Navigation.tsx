832060549f2fa25fc6bd013ff7151e69b4276d86
kdando <kvdando@gmail.com>
2024-04-08 22:55:19 +0100

    renaming pages

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -14,12 +14,16 @@ export function Navigation () {
     const [isRouting, setIsRouting] = useState(false);
     const [prevPath, setPrevPath] = useState("/");
 
+    //if previous path, i.e. last page, does not equal current pathname, i.e. page we are on, then set isRouting to true...
+    //check this whenever path or previous path change
     useEffect(() => {
         if (prevPath !== path) {
             setIsRouting(true);
         }
     }, [path, prevPath])
 
+    //if isRouting is true, set previous path to equal current pathname, then, after a timeout, set isRouting to false.
+    //the timeout is space for the transition animation to play
     useEffect(() => {
         if (isRouting === true) {
             setPrevPath(path);
@@ -32,10 +36,14 @@ export function Navigation () {
     
 
    return (
+    
+    // render the div that actually contains/"is" the navigation bar
     <div  style={{left: "20%"}} className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] flex-full justify-between items-center border border-white py-1 px-4 bg-black">
 
+        {/* if isRouting is true we render the Transition animation */}
         {isRouting && <Transition />}
 
+        {/* then we render the navigation bar links within the containing div */}
         {NavLinks.map((nav) => (
             <Link key={nav.name} href={nav.link} className="mb-16 pl-4 min-w-[20%]">
                 <nav.icon className={`w-[24px] h-[24px] ${path === nav.name ? "text-purple-800" : "text-white"}`} />
--- components/Transition.tsx
+++ components/Transition.tsx
@@ -22,7 +22,7 @@ export function Transition () {
   return (
     <div>
         <motion.div 
-        className='fixed right-0 h-screen w-screen bottom-full z-[30] bg-[#2e2257]'
+        className='fixed right-0 h-screen w-screen bottom-full z-[30] bg-green-500'
         variants={TransitionVariants}
         initial="initial"
         exit="exit"
@@ -30,7 +30,7 @@ export function Transition () {
         transition={{delay: 0.2, duration: 0.6, ease: "easeInOut"}} />
 
         <motion.div 
-        className='fixed right-0 h-screen w-screen bottom-full z-[20] bg-[#3b2d71]'
+        className='fixed right-0 h-screen w-screen bottom-full z-[20] bg-red-500'
         variants={TransitionVariants}
         initial="initial"
         exit="exit"
@@ -38,7 +38,7 @@ export function Transition () {
         transition={{delay: 0.4, duration: 0.6, ease: "easeInOut"}} />
 
         <motion.div 
-        className='fixed right-0 h-screen w-screen bottom-full z-[10] bg-[#4b3792]'
+        className='fixed right-0 h-screen w-screen bottom-full z-[10] bg-white'
         variants={TransitionVariants}
         initial="intial"
         exit="exit"
